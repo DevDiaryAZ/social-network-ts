d7e6247fcab53d26b69b913670712a0d3016c4ea
Alenka <alenazarubi@gmail.com>
2022-07-31 00:36:58 +0300

    stylized the Dialogs.tsx component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -26,20 +26,18 @@ export const Dialogs = () => {
                         </div>
                     </div>
 
-                    <div className="recipients-list">
-                        <div className="recipient-avatar">
-                            <img src="/" alt=""/>
-                            <div className="presence-entity">
-                                            <span className="visually-hidden">
-                                                Status is online
-                                            </span>
+                    <div className={s.recipients__list}>
+                        <div className={s.recipients__item}>
+                            <div className={s.recipients__avatar}>
+                                <img src="https://pressa.tv/uploads/posts/2018-08/1535007604_9.jpg" alt="image"/>
                             </div>
-                        </div>
-                        <div className="right-part">
-                            <div className="last-sent" title="1 w">15m</div>
-                            <span className="user-recipient-name">Jassica William</span>
-                            <p> contact me on my email address i sent to you so that i can tell you
-                                more about myself</p>
+                            <div className={s.recipients__itemContent}>
+                                <div className={s.recipients__nameWrapper}>
+                                    <h6 className={s.recipients__name}>Leonarda</h6>
+                                    <p className={s.recipients__time}>5 minutes ago</p>
+                                </div>
+                                <p className={s.recipients__message}>Well, it seems to be working now.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -47,7 +45,55 @@ export const Dialogs = () => {
             </div>
 
             {/* Messages */}
-            <div className={s.messagesBody}>
+            <div className={s.messages}>
+                {/* Message header */}
+                <div className={s.messages__user}>
+                    <div className={s.messages__avatar}>
+                        <img src="https://pressa.tv/uploads/posts/2018-08/1535007604_9.jpg" alt="image"/>
+                    </div>
+                    <div className={s.messages__itemContent}>
+                        <div className={s.messages__nameWrapper}>
+                            <h6 className={s.messages__name}>Leonarda</h6>
+                            <p className={s.messages__time}>5 minutes ago</p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Message chat */}
+                <div className={s.messages__chatContainer}>
+                    <div className={s.messages__chatContent}>
+                        <ul className={s.messages__chatsLists}>
+                            <li className={s.messages__you}>
+                                <div className={s.messages__chatThumb}>
+                                    <img src="https://pressa.tv/uploads/posts/2018-08/1535007604_9.jpg" alt=""/>
+                                </div>
+                                <div className={s.messages__text}>
+                                    <span>
+                                    Hi, Peter, How are you today?.
+                                    </span>
+                                </div>
+                            </li>
+                            <li className={s.messages__me}>
+                                <div className={s.messages__chatThumb}>
+                                    <img src="https://lofficiel.imgix.net/production/russia/images/1531997317085199-b66a7d6e3d6f3d27fa5fd8531f8e8fe3.jpg?w=450&h=800&fit=clip&crop=faces&auto=%5B%22format%22%2C%20%22compress%22%5D&cs=srgb" alt=""/>
+                                </div>
+                                <div className={s.messages__text}>
+                                    <span>
+                                    Hi, Leo, I am fine but about you?
+                                    </span>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                {/* Message form */}
+                <form>
+                    <div className={s.form__wrapper}>
+                        <input type="text" className={`input ${s.form__input}`}/>
+                        <button type="submit" className={`button button_blue ${s.form__button}`}>Send</button>
+                    </div>
+                </form>
             </div>
         </section>
     )
